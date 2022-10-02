4422285ed624082358cca1283513a08913d696c6
Ryan James <rjames@rjames-mbpr-it.lan>
2022-10-02 08:57:40 -0400

    updated styling on myposts (-form)

--- pages/myPosts.js
+++ pages/myPosts.js
@@ -12,7 +12,7 @@ export const getStaticProps = async (context) => {
 
     const query = gql`
     query MyQuery {
-    posts {
+    posts (orderBy: updatedAt_DESC) {
       id
       body
       date
@@ -44,17 +44,46 @@ export default function MyPosts ({ posts }) {
 
     return (
         <>
+            <Meta />
             <Header />
-            <div className="container">
+            
+
+
+            <div className="container px-2">
+
+                <div className="flex flex-col items-center justify-center">
                 {posts.map(post => (
                     loggedInUser == post.userName ? (
-                        <div key={post.id} className="py-4">
-                        <h1>{post.body}</h1>
-                        <p>{post.date.split("T")[0]}</p>
-                        <p>{post.userName}</p>
+                <div key={post.id} class="max-w-sm w-full lg:w-screen lg:flex py-6">
+                <div className="pt-10 border-l border-gray-400 border-t border-b h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden">
+                    <h2 className="text-8xl">{post.userName.substring(0,2).toUpperCase()}</h2>
+                </div>
+                <div className="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+                    <div className="mb-8 w-96">
+                    <p className="text-sm text-gray-600 flex items-center">
+                        <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+                        </svg>
+                        Subscriptions Only
+                    </p>
+                    <div className="text-gray-900 font-bold text-xl mb-2">New Post by {post.userName}</div>
+                    <p className="text-gray-700 text-base">{post.body}</p>
                     </div>
+                    <div className="flex items-center">
+                    <div className="text-sm">
+                        <p className="text-gray-900 leading-none">{post.userName}</p>
+                        <p className="text-gray-600">{post.date.split("T")[0]}</p>
+                        <p className="text-gray-600">Location: {post.location}</p>
+                        <p className="text-gray-600">Info: {post.userInfo}</p>
+                    </div>
+                    </div>
+                </div>
+                </div>    
                     ) : null
-                ))}    
+                ))} 
+                </div>
+
+
             </div>
 
             <PostsForm session={session} />
