ab3df85aeccc36dd46cfbbad3c7a9be235b1eb61
Ryan James <rjames@rjames-mbpr-it.cdschool.org>
2022-10-05 07:59:26 -0400

    deploy fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
+
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -10,6 +12,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  
 }
 
 export default NextAuth(authOptions)
