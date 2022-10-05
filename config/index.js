8d17e7467eb074e220aa4740163b7d2b9d0758c5
Ryan James <rjames@rjames-mbpr-it.cdschool.org>
2022-10-05 08:54:39 -0400

    working on deploy

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://cds-social.vercel.app';
+// export const server = dev ? 'http://localhost:3000' : 'https://cds-social.vercel.app';
+export const server = 'https://cds-social.vercel.app';
