b3e29d934725517ce28570bfba363f92a723c298
Ryan James <rjames@rjames-mbpr-it.lan>
2022-10-07 11:57:29 -0400

    swr working locally

--- pages/myPosts.js
+++ pages/myPosts.js
@@ -5,54 +5,66 @@ import Meta from "../components/meta"
 import { useRouter } from 'next/router'
 import { deleteComment } from '../services'
 import EachPost from "../components/eachPost"
+import useSWR from 'swr'
 
 
 import { gql, GraphQLClient } from 'graphql-request';
 
 
-export const getStaticProps = async (context) => {
+// export const getStaticProps = async (context) => {
     
 
-    const query = gql`
-    query MyQuery {
-    posts (orderBy: updatedAt_DESC) {
-      id
-      body
-      date
-      userName
-      location
-      userInfo
-      documentInStages {
-        stage
-      }
+//     const query = gql`
+//     query MyQuery {
+//     posts (orderBy: updatedAt_DESC) {
+//       id
+//       body
+//       date
+//       userName
+//       location
+//       userInfo
+//       documentInStages {
+//         stage
+//       }
+//     }
+//   }
+// `
+
+//     const graphQLClient = new GraphQLClient(process.env.HYGRAPH_ENDPOINT)
+//     const data = await graphQLClient.request(query)
+//     return {
+//         props: {
+//             posts: data.posts
+//         },
+//         revalidate: 1,
+//     }
+// }
+
+
+
+export async function getStaticProps() {
+    const { getPosts } = require("../lib/helpers")
+    return {
+      props: (await getPosts()), revalidate: 1
     }
   }
-`
-
-    const graphQLClient = new GraphQLClient(process.env.HYGRAPH_ENDPOINT)
-    const data = await graphQLClient.request(query)
-    return {
-        props: {
-            posts: data.posts
-        },
-        revalidate: 1,
-    }
-}
 
 
 
 
 
-
-
-
-
-const MyPosts = ({ posts }) => {
+const MyPosts = (props) => {
     const { data: session } = useSession()
     const loggedInUser = session?.user?.email.split("@")[0] || null
 
+    const fetcher = (...args) => fetch(...args).then(res => res.json())
+    const { data }= useSWR('/api', fetcher, {fallbackData: props, refreshInterval: 30000})
+    let posts = data.posts
+    const router = useRouter()
     
-    
+    const refreshData = () => { 
+        router.replace(router.asPath)
+    }
     
 
     return (
