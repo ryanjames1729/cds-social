52e05bf22a8b609a6c061a97e5a633f0e615beb9
Ryan James <rjames@rjames-mbpr-it.cdschool.org>
2022-10-11 11:55:10 -0400

    updated swr for admin

--- pages/admin.js
+++ pages/admin.js
@@ -4,38 +4,52 @@ import Header from "../components/header"
 import Meta from "../components/meta"
 import { gql, GraphQLClient } from 'graphql-request';
 import { useRouter } from 'next/router'
+import useSWR from "swr";
 import EachPost from "../components/eachPost"
 
-export const getStaticProps = async (context) => {    
+// export const getStaticProps = async (context) => {    
 
-    const query = gql`
-    query MyQuery {
-    posts (orderBy: updatedAt_DESC, stage: DRAFT) {
-      id
-      body
-      date
-      userName
-      location
-      userInfo
-      documentInStages {
-        stage
-      }
+//     const query = gql`
+//     query MyQuery {
+//     posts (orderBy: updatedAt_DESC, stage: DRAFT) {
+//       id
+//       body
+//       date
+//       userName
+//       location
+//       userInfo
+//       documentInStages {
+//         stage
+//       }
+//     }
+//   }
+// `
+//     const graphQLClient = new GraphQLClient(process.env.HYGRAPH_ENDPOINT)
+//     const data = await graphQLClient.request(query)
+
+//     return {
+//         props: {
+//             posts: data.posts
+//         },
+//         revalidate: 1,
+//     }
+// }
+
+export async function getStaticProps() {
+    const { getPosts } = require("../lib/helpers")
+    return {
+      props: (await getPosts()), revalidate: 1
     }
   }
-`
-    const graphQLClient = new GraphQLClient(process.env.HYGRAPH_ENDPOINT)
-    const data = await graphQLClient.request(query)
 
-    return {
-        props: {
-            posts: data.posts
-        },
-        revalidate: 1,
-    }
-}
+export default function Admin (props) {
+    // const { data: session } = useSession()
 
-const Admin = ({ posts }) => {
-    const { data: session } = useSession()
+    const fetcher = (...args) => fetch(...args).then(res => res.json())
+    const { data } = useSWR('/api', fetcher, {fallbackData: props, refreshInterval: 30000})
+    let posts = data.posts
+    const router = useRouter()
+
     const loggedInUser = session?.user?.email.split("@")[0] || null
 
     
@@ -66,4 +80,3 @@ const Admin = ({ posts }) => {
     )
 }
 
-export default Admin
